8d478ca6fac943622dc428ac2b4c7a0831eca430
hallymjw-wq <hallymjw@gmail.com>
2026-09-23 17:48:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-const CACHE="v6-drive-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="v6-drive-v11";
+const ASSETS=["./icon-180.png","./icon-512.png","./manifest.webmanifest"];
+
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
